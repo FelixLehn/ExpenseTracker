becda0f1230b56333fb4a1c9cbd33098097ccaaf
FelixLehn <70092091+FelixLehn@users.noreply.github.com>
2020-12-30 17:14:38 +0100

    Updated build.py

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ use_plugin("python.unittest")
 use_plugin("python.flake8")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
-
+use_plugin("python.sonarqube")
 
 name = "ExpenseTracker"
 version="1.0"
@@ -25,3 +25,9 @@ default_task = "publish"
 @init
 def set_properties(project):
     project.set_property("coverage_break_build", False)
+    project.set_property("flake8_include_test_sources",True)
+
+@init
+def set_sonarqube(project):
+    project.set_property("sonarqube_project_name",'Expensetracker')
+
